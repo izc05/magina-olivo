f80bda6e1cd567a3369df76dd0b0ef0c7e186a28
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-03 08:07:43 +0200

    refine: load profile V2.8 polish

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './styles/polish-v23.css';
 import './styles/market-v25.css';
 import './styles/journal-v26.css';
 import './styles/news-v27.css';
+import './styles/profile-v28.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

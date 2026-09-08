6f853b78571d65bb7598e7244a4e820ea72431df
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-08 16:47:46 +0200

    style(directory): load curated media styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -37,6 +37,7 @@ import './field-notebook.css';
 import './delivery-entry.css';
 import './offline-cold-start.css';
 import './magina-directory.css';
+import './magina-directory-media.css';
 import './magina-weather.css';
 import './magina-hub.css';
 import './magina-market.css';

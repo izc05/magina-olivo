9544f20481ba6faa0e070e47ca03cad6ff138d60
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 14:22:50 +0200

    map: initialize MapLibre at app startup

--- app/src/main/java/com/isivolt/maginaolivo/MaginaOlivoApplication.kt
+++ app/src/main/java/com/isivolt/maginaolivo/MaginaOlivoApplication.kt
@@ -3,8 +3,14 @@ package com.isivolt.maginaolivo
 import android.app.Application
 import androidx.room.Room
 import com.isivolt.maginaolivo.data.local.MaginaOlivoDatabase
+import org.maplibre.android.MapLibre
 
 class MaginaOlivoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MapLibre.getInstance(this)
+    }
+
     val database: MaginaOlivoDatabase by lazy {
         Room.databaseBuilder(
             applicationContext,

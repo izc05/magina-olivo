4adf99f9c8246e1fc65a59dcd67d7b7008ee7053
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 19:28:49 +0200

    feat(weather): add RainAlertModels.kt

---
+++ app/src/main/java/com/isivolt/maginaolivo/domain/weather/RainAlertModels.kt
@@ -0,0 +1,20 @@
+package com.isivolt.maginaolivo.domain.weather
+
+enum class RainAlertLevel {
+    NOTICE,
+    HIGH,
+}
+
+data class RainAlert(
+    val date: String,
+    val precipitationProbabilityPercent: Int,
+    val level: RainAlertLevel,
+    val municipality: WeatherMunicipality,
+)
+
+data class RainAlertSettings(
+    val notificationsEnabled: Boolean = false,
+    val municipalityCode: String = MaginaWeatherMunicipalities.default.code,
+    val thresholdPercent: Int = 60,
+    val horizonDays: Int = 2,
+)

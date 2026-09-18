350568a39efa75bee767a05b01030765008b1dc1
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 14:17:32 +0200

    android: opt in to Material3 top app bar

--- app/src/main/java/com/isivolt/maginaolivo/ui/MaginaOlivoApp.kt
+++ app/src/main/java/com/isivolt/maginaolivo/ui/MaginaOlivoApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.isivolt.maginaolivo.data.remote.SupabaseProvider
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MaginaOlivoApp() {
     MaterialTheme {

f3636c1d5c0398b9a3caa6f701ee43fb3f381a9b
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-19 07:10:14 +0200

    fix(ui): restore MaterialTheme import in shell

--- app/src/main/java/com/isivolt/maginaolivo/ui/shell/MaginaAppShell.kt
+++ app/src/main/java/com/isivolt/maginaolivo/ui/shell/MaginaAppShell.kt
@@ -3,6 +3,7 @@ package com.isivolt.maginaolivo.ui.shell
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
